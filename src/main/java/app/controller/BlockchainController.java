73af3b53d21f9578c91e24dd1e71ba4b28b71de8
devssh <devasood@gmail.com>
2018-04-27 22:27:47 +0530

    Add cors support to ui

--- src/main/java/app/controller/BlockchainController.java
+++ src/main/java/app/controller/BlockchainController.java
@@ -1,6 +1,7 @@
 package app.controller;
 
 import app.model.KeysView;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,9 @@ import static app.service.KeyzManager.KEYS;
 import static app.service.MailService.SendMailWithConfirmationCode;
 import static app.utils.JsonUtils.ToJSON;
 
+@CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
 public class BlockchainController {
-
     @GetMapping(value = "/keys")
     public String keys() {
         return ToJSON(new KeysView(KEYS));
